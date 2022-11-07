3e0597610332b017d5beaa24e376d0838ebe638f
Taylher <alexmota@alu.ufc.br>
2022-11-07 14:38:56 -0300

    removendo codigo desnecessario

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import SwaggerUI from "swagger-ui-express";
 import { swaggerBasicInfo } from "./docs";
 import { userRouter } from "./modules/user/routes/user.route";
-import { authRoutes } from "./modules/authentication/routes/authentication.routes"
+import { authRoutes } from "./modules/authentication/routes/authentication.routes";
 const app = express();
 
 // Middlewares
@@ -14,16 +14,6 @@ app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerBasicInfo));
 
 // Routes
 app.use("/api/v1/users", userRouter);
-
-type UserRegisterData = {
-    name: string;
-    email: string;
-    password: string;
-    avatar: string;
-};
-
-
 app.use("/api/v1/auth", authRoutes);
-
 
 export { app };
