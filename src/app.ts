fd70a81c597fc2650eaa569da1179e82d672e777
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-11-10 07:33:23 -0300

    remove tipo de dados, importações e comentários desnecessários no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,20 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import SwaggerUI from "swagger-ui-express";
 import { swaggerBasicInfo } from "./docs";
+
 import { userRouter } from "./modules/user/routes/user.route";
-import { authRoutes } from "./modules/authentication/routes/authentication.routes"
+import { authRoutes } from "./modules/authentication/routes/authentication.routes";
+
 const app = express();
 
 // Middlewares
 app.use(express.json());
-// app.use(jwt);
 
 // Documentation
 app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerBasicInfo));
 
 // Routes
 app.use("/api/v1/users", userRouter);
-
-type UserRegisterData = {
-    name: string;
-    email: string;
-    password: string;
-    avatar: string;
-};
-
-
 app.use("/api/v1/auth", authRoutes);
-
 
 export { app };
