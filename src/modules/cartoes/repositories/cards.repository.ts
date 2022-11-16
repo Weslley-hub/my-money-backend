12c1de8c2be59285c70b535a12514df0459c6f76
Taylher <alexmota@alu.ufc.br>
2022-11-16 16:34:16 -0300

    Completando as funcoes do CardRepository

--- src/modules/cartoes/repositories/cards.repository.ts
+++ src/modules/cartoes/repositories/cards.repository.ts
@@ -2,7 +2,26 @@ import { dbConnection } from "../../../database";
 import { CreateCardDto } from "../dto/create-cards.dto";
 
 export class CardRepository {
-  registerCard(card: CreateCardDto) {
+  save(card: CreateCardDto) {
     return dbConnection<CreateCardDto>("cards").insert(card);
+  }
+  update(card: CreateCardDto) {
+    return dbConnection<CreateCardDto>("cards")
+      .update(card)
+      .where("id", "=", card.id);
+  }
+  delete(id: string) {
+    return dbConnection<CreateCardDto>("cards").delete().where("id", "=", id);
+  }
+  findByUserId(user_id: string) {
+    return dbConnection<CreateCardDto>("cards")
+      .select("*")
+      .where("user_id", "=", user_id)
+      .first();
+  }
+  findAllByUserId(user_id: string) {
+    return dbConnection<CreateCardDto>("cards")
+      .select("*")
+      .where("user_id", "=", user_id);
   }
 }
