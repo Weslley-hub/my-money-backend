70a513de7177acd618e1902194ca2eaf81624be2
Taylher <alexmota@alu.ufc.br>
2022-11-17 22:57:55 -0300

    impedindo o cadastro de cards com numbers iguais

--- src/modules/cartoes/services/cards.service.ts
+++ src/modules/cartoes/services/cards.service.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+import { BusinessException } from "../../api/exceptions/business.exception";
 import { CreateCardDto } from "../dto/create-cards.dto";
 import { CardRepository } from "../repositories/cards.repository";
 import { CardValidationSchema } from "../validation/cards.schema";
@@ -9,7 +11,22 @@ class CardsService {
     await CardValidationSchema.validate(cardData, {
       abortEarly: false,
     });
-    await cardRepository.save(cardData);
+    const carId = uuidv4();
+    const card = await cardRepository.findById(cardData.number);
+    if (card) {
+      throw new BusinessException(
+        `Ja existe um cartão cadastrado com o número: ${cardData.number}`
+      );
+    }
+    await cardRepository.save({
+      id: carId,
+      name: cardData.name,
+      number: cardData.number,
+      type: cardData.type,
+      flag: cardData.flag,
+      limit: cardData.limit,
+      user_id: cardData.user_id,
+    });
   }
 }
 
