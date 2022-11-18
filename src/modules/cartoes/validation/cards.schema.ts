36951d8ee1aacf15e8f56966bfb83bf30640ab3f
Taylher <alexmota@alu.ufc.br>
2022-11-17 22:58:23 -0300

    mudando a validação, o id não é necessario.

--- src/modules/cartoes/validation/cards.schema.ts
+++ src/modules/cartoes/validation/cards.schema.ts
@@ -2,7 +2,6 @@ import * as Yup from "yup";
 
 const CardValidationSchema = Yup.object().shape({
   name: Yup.string().required("Nome é obrigatorio"),
-  id: Yup.string().required("O id é obrigatorio"),
   number: Yup.string().required("O numero é obrigatorio"),
   type: Yup.string().required("O tipo é obrigatorio"),
   flag: Yup.string().required("A bandeira é obrigatoria"),
