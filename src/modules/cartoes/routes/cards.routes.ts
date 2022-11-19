59e5112dff6d72fc325414af5bac91ac05d52201
Taylher <alexmota@alu.ufc.br>
2022-11-19 05:43:42 -0300

    sintetizando card routes e separando as tarefas (controller)

--- src/modules/cartoes/routes/cards.routes.ts
+++ src/modules/cartoes/routes/cards.routes.ts
@@ -10,40 +10,10 @@ const cardRouter = Router();
 const cardController = new CardController();
 cardRouter.post("/register", cardController.register);
 
-cardRouter.post("/current-user-cards", async (req, res) => {
-  const { id } = req.body;
-  const cardRepository = new CardRepository();
-  const cartoes = await cardRepository.findAllByUserId(id);
-  return res.status(200).json({ cartoes });
-});
+cardRouter.post("/current-user-cards", cardController.userCardList);
 
-cardRouter.post("/delete", async (req, res) => {
-  const cardData = req.body as DeleteCardDto;
-  const cardRepository = new CardRepository();
-  await CardValidationDelete.validate(cardData);
+cardRouter.post("/delete", cardController.delete);
 
-  const existingCardById = await cardRepository.findById(cardData.id);
-  console.log("existingCardById - ", existingCardById);
-  if (!existingCardById) {
-    return res.status(400).json("Não existe um cartão com esse id.");
-  }
-
-  await cardRepository.delete(cardData.id);
-  return res.status(200).json({ message: "Cartão excluido" });
-});
-
-cardRouter.post("/update", async (req, res) => {
-  const cardData = req.body as RepositoryCardDto;
-  await CardValidationUpdate.validate(cardData);
-  console.log(cardData);
-  const cardRepository = new CardRepository();
-  const existingCardById = await cardRepository.findById(cardData.id);
-  console.log("existingCardById - ", existingCardById);
-  if (!existingCardById) {
-    return res.status(400).json("Não existe um cartão com esse id.");
-  }
-  await cardRepository.update(cardData);
-  return res.status(200).json({ message: "Cartão foi atualizado" });
-});
+cardRouter.post("/update", cardController.update);
 
 export { cardRouter };
