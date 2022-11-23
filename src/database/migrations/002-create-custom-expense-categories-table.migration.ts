6271b26b30b4957828bc52afd797fc6d054f51c4
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-11-23 11:26:32 -0300

    adiciona migration que cria a tabela de categorias de gastos customizadas e adiciona função createIfNotExists para validação da criação de tabelas

---
+++ src/database/migrations/002-create-custom-expense-categories-table.migration.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+import { createTableIfNotExists } from "../utils";
+
+const TABLE_NAME = "custom_expense_categories";
+
+export async function up(knex: Knex) {
+  return createTableIfNotExists(TABLE_NAME, knex, createTable);
+}
+
+function createTable(knex: Knex) {
+  return knex.schema.withSchema("public").createTable(TABLE_NAME, (table) => {
+    table.string("id").primary();
+    table.string("name").notNullable();
+    table.string("icon", 1).notNullable();
+
+    table.string("user_id").notNullable();
+    table.foreign("user_id").references("id").inTable("users");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(TABLE_NAME);
+}
