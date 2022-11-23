27a6442535558f1054891ea4cf5f56d2af4cc6e7
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-11-23 20:21:37 -0300

    adiciona exportação dos tipos dos erros da api

---
+++ src/modules/api/types/index.ts
@@ -0,0 +1,5 @@
+export * from "./api-error-response.type";
+export * from "./api-error.type";
+export * from "./api-response.type";
+export * from "./status-code.type";
+export * from "./validation-field-error.type";
