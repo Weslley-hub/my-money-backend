9918e71604c600ad212ff69bc27f0a20c8720248
Taylher <alexmota@alu.ufc.br>
2022-11-24 14:13:37 -0300

    removi importação

--- src/modules/card/services/card.service.ts
+++ src/modules/card/services/card.service.ts
@@ -2,14 +2,10 @@ import { v4 as uuidv4 } from "uuid";
 import { BusinessException } from "../../api/exceptions/business.exception";
 import { UserRepository } from "../../user/repositories/user.repository";
 import { CreateCardDto } from "../dto/create-cards.dto";
-import { DeleteCardDto } from "../dto/delete-cards.dto";
 import { RepositoryCardDto } from "../dto/repository-cards.dto";
-import { UserCardList } from "../dto/user-card-list-dto";
 import { CardRepository } from "../repositories/card.repository";
-import { CardValidationDelete } from "../validation/card.validation.delete";
 import { CardValidationUpdate } from "../validation/card.validation.update";
 import { CardValidationSchema } from "../validation/cards.validation.schema";
-import { UserCardListValidation } from "../validation/user.card.list.validation";
 
 const cardRepository = new CardRepository();
 const userRepository = new UserRepository();
