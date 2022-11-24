ecbc7ae7d8c277c15351863c207df76dd0a57033
Taylher <alexmota@alu.ufc.br>
2022-11-24 01:34:25 -0300

    completando os itens do validation card

--- src/modules/cartoes/validation/card.validation.update.ts
+++ src/modules/cartoes/validation/card.validation.update.ts
@@ -2,12 +2,13 @@ import * as Yup from "yup";
 
 const CardValidationUpdate = Yup.object().shape({
   id: Yup.string().required("O id é obrigatorio"),
-  user_id: Yup.string().required("O user_id é obrigatorio"),
   name: Yup.string().required("Nome é obrigatorio"),
+  user_id: Yup.string().required("O user_id é obrigatorio"),
   number: Yup.string().required("O numero é obrigatorio"),
   type: Yup.string().required("O tipo é obrigatorio"),
   flag: Yup.string().required("A bandeira é obrigatoria"),
   limit: Yup.string().required("O limite é obrigatorio"),
+  invoice_day: Yup.string().required("O dia da fatura é obrigatório"),
 });
 
 export { CardValidationUpdate };
