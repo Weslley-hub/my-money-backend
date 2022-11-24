02ae8f36cc75cba1458deb0279052e15b066f25b
Taylher <alexmota@alu.ufc.br>
2022-11-24 01:38:06 -0300

    removendo tipo desnecessário

--- src/modules/card/validation/user.card.list.validation.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-const UserCardListValidation = Yup.object().shape({
-  id: Yup.string().required("O id é obrigatorio"),
-});
-user.card.list.validation;
-export { UserCardListValidation };
