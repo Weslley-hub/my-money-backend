07c3eefe135f970dbfbbebfe00532f1b4961a6a7
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-11-25 12:58:03 -0300

    remove módulo de documentação e swagger-ui

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import SwaggerUI from "swagger-ui-express";
-import { swaggerBasicInfo } from "./docs";
 
 import { userRouter } from "./modules/user/routes/user.route";
 import { authRoutes } from "./modules/authentication/routes/authentication.routes";
@@ -11,13 +9,9 @@ const app = express();
 // Middlewares
 app.use(express.json());
 
-// Documentation
-app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerBasicInfo));
-
 // Routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/cards", cardRouter);
-
 
 export { app };
