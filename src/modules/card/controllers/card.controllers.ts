2e8b237101db5a9a57fb923ad2422be48420e399
Taylher <alexmota@alu.ufc.br>
2022-11-27 16:44:24 -0300

    adicionando a modularizacao dos try caths -tryExemple

--- src/modules/card/controllers/card.controllers.ts
+++ src/modules/card/controllers/card.controllers.ts
@@ -30,79 +30,80 @@ class CardController {
     });
   }
 
-  
   async list(request: Request, response: Response) {
+    const userId = request.query.userId as string;
     try {
-      const userId = request.query.userId as string;
-
-      const cards = await cardsService.list(userId);
-      return response.status(StatusCode.SUCCESS).json({ cards });
+      const apiResponse = await this.tryList(userId, response);
+      return apiResponse;
     } catch (error) {
-      const apiErrorResponse =
-        ExceptionHandler.parseErrorAndGetApiResponse(error);
-
-      return response
-        .status(apiErrorResponse.statusCode)
-        .json(apiErrorResponse);
+      return this.cathPattern(error, response);
     }
   }
 
-  private async tryList(request: Request, response: Response) {
-    const cardId = request.body as string;
-    const cards = await cardsService.list(cardId);
+  private async tryList(userId:string, response: Response) {
+    const cards = await cardsService.list(userId);
     return response.status(StatusCode.SUCCESS).json({ cards });
   }
 
-  async delete(request: Request, response: Response) {
+  async uniqueListing(request:Request, response:Response){
+    const userId = request.query.userId as string;
     try {
-      const cardId = request.query.cardId as string;
-      await cardsService.delete(cardId);
-      return response.status(200).json({ message: "Cartão excluido" });
+      const apiResponse = this.tryUniqueListing(userId,response);
+      return apiResponse;
     } catch (error) {
-      const apiErrorResponse =
-        ExceptionHandler.parseErrorAndGetApiResponse(error);
+      return this.cathPattern(error, response);
+    }
+  }
+  private async tryUniqueListing(userId:string, response:Response){
+    const card = await cardsService.uniqueListing(userId);
+    return response.status(StatusCode.SUCCESS).json({ card });
+  }
 
-      return response
-        .status(apiErrorResponse.statusCode)
-        .json(apiErrorResponse);
+  async delete(request: Request, response: Response) {
+    const cardId = request.query.cardId as string;
+    try {
+      const apiResponse = this.tryDelete(cardId,response);
+      return apiResponse;
+    } catch (error) {
+      return this.cathPattern(error, response);
     }
   }
 
-  private async tryDelete(request: Request, response: Response) {
-    const cardId = request.body as string;
+  private async tryDelete(cardId: string, response: Response) {
     await cardsService.delete(cardId);
-    return response.status(200).json({ message: "Cartão excluido" });
+      return response.status(200).json({ message: "Cartão excluido" });
   }
 
   async update(request: Request, response: Response) {
+    const cardData = request.body as RepositoryCardCreditDto;
     try {
-      const cardData = request.body as RepositoryCardCreditDto;
-      await cardsService.update(cardData);
-      return response.status(200).json({ message: "Cartão foi atualizado" });
-    } catch (error) {
-      const apiErrorResponse =
-        ExceptionHandler.parseErrorAndGetApiResponse(error);
-
-      return response
-        .status(apiErrorResponse.statusCode)
-        .json(apiErrorResponse);
+      const apiResponse = await this.tryUpdate(cardData, response);
+      return apiResponse;
+    }
+    catch (error) {
+      return this.cathPattern(error,response);
     }
   }
 
-  private async tryUpdate(request: Request, response: Response) {
-    const cardData = request.body as RepositoryCardCreditDto;
-    await cardsService.update(cardData);
+  private async tryUpdate(cardData: RepositoryCardCreditDto, response: Response) {
+    await cardsService.update({
+      id: cardData.id,
+      name: cardData.name,
+      number: cardData.number,
+      flag: cardData.flag,
+      type: cardData.type,
+      limit: cardData.limit,
+      invoiceAmount: cardData.invoice_amount,
+      invoiceDay: cardData.invoice_day,
+      userId: cardData.user_id,
+
+    });
     return response.status(200).json({ message: "Cartão foi atualizado" });
-  }
-  private handleApiErrorResponse(error: unknown, response: Response) {
-    const apiResponse = ExceptionHandler.parseErrorAndGetApiResponse(error);
-    return response.status(apiResponse.statusCode).json(apiResponse);
   }
 
   private async cathPattern(error: unknown, response: Response) {
     const apiErrorResponse =
       ExceptionHandler.parseErrorAndGetApiResponse(error);
-  
     return response.status(apiErrorResponse.statusCode).json(apiErrorResponse);
   }
 }
