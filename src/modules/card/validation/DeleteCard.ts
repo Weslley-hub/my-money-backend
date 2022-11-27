a98648e0c8a32ca01363d53b1201af8e4c2be0ea
Taylher <alexmota@alu.ufc.br>
2022-11-27 18:30:10 -0300

    removendo arquivos com nomes incorretos

--- src/modules/card/validation/DeleteCard.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-const CardValidationDelete = Yup.object().shape({
-  id: Yup.string().required("O id é obrigatorio"),
-});
-
-export { CardValidationDelete };
--- src/modules/card/validation/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Card";
-export * from "./DeleteCard";
-export * from "./UpdateCard";
