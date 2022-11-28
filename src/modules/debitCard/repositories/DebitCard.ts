01756879ddf0713662468dd3cc8e6efd43514130
Taylher <alexmota@alu.ufc.br>
2022-11-28 14:06:47 -0300

    refatorando o repository

--- src/modules/debitCard/repositories/DebitCard.ts
+++ src/modules/debitCard/repositories/DebitCard.ts
@@ -7,32 +7,32 @@ export class CardRepository {
   }
 
   updateDebit(card: RepositoryCardDebitDto) {
-    return dbConnection<RepositoryCardCreditDto>("debit_cards")
+    return dbConnection<RepositoryCardDebitDto>("debit_cards")
       .where("id", "=", card.id)
       .update(card);
   }
 
   deleteDebit(id: string) {
-    return dbConnection<RepositoryCardCreditDto>("debit_cards")
+    return dbConnection<RepositoryCardDebitDto>("debit_cards")
       .delete()
       .where("id", "=", id);
   }
 
   findAllDebitByUserId(user_id: string) {
-    return dbConnection<RepositoryCardCreditDto>("debit_cards")
+    return dbConnection<RepositoryCardDebitDto>("debit_cards")
       .select("*")
       .where("user_id", "=", user_id);
   }
 
   findDebitById(id: string) {
-    return dbConnection<RepositoryCardCreditDto>("debit_cards")
+    return dbConnection<RepositoryCardDebitDto>("debit_cards")
       .select("*")
       .where("id", "=", id)
       .first();
   }
 
   findByNumberDebit(number: number) {
-    return dbConnection<RepositoryCardCreditDto>("debit_cards")
+    return dbConnection<RepositoryCardDebitDto>("debit_cards")
       .select("*")
       .where("number", "=", number)
       .first();
