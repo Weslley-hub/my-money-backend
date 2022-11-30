3e8216806ff4b58134b685e536b1c98deb56d02d
Taylher <alexmota@alu.ufc.br>
2022-11-29 22:54:30 -0300

    alterando o validation, o type não é necessario

--- src/modules/debitCard/validation/UpdateDebitCard.ts
+++ src/modules/debitCard/validation/UpdateDebitCard.ts
@@ -4,8 +4,7 @@ const CardDebitValidationUpdate = Yup.object().shape({
   id: Yup.string().required("O id é obrigatorio"),
   name: Yup.string().required("Nome é obrigatorio"),
   userId: Yup.string().required("O user_id é obrigatorio"),
-  number: Yup.string().required("O numero é obrigatorio"),
-  type: Yup.string().required("O tipo é obrigatorio")
+  number: Yup.string().required("O numero é obrigatorio")
 });
 
 export { CardDebitValidationUpdate };
