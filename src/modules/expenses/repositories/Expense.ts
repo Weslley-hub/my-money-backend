e5a213824b1e58a8931a45da293a0ce55ebe9633
Taylher <alexmota@alu.ufc.br>
2022-12-07 14:56:11 -0300

    criando funcao de listagem de expenses no repository

--- src/modules/expenses/repositories/Expense.ts
+++ src/modules/expenses/repositories/Expense.ts
@@ -5,4 +5,11 @@ export class ExpenseRepository {
   create(expense: UpdateExpenseRepository) {
     return dbConnection<UpdateExpenseRepository>("expenses").insert(expense);
   }
+  findExpenseById(user_id: string) {
+    return dbConnection<UpdateExpenseRepository>("expenses")
+      .select("*")
+      .where("user_id", "=", user_id)
+      .first();
+  }
+  }
 }
